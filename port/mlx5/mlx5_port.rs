375937091efee5748cbf0794a666c4dde8c6f50c
Gregory Etelson <getelson@nvidia.com>
2025-04-06 13:56:09 +0300

    rust: implement  Mlx5Port with Rx functionality

---
+++ port/mlx5/mlx5_port.rs
@@ -0,0 +1,88 @@
+use std::marker::PhantomData;
+use std::os::raw::c_void;
+use rdpdk::dpdk_raw::ethdev_driver::{rte_eth_dev};
+use rdpdk::dpdk_raw::rte_ethdev::{rust_get_port_eth_device};
+use rdpdk::dpdk_raw::rte_mbuf::rte_mbuf;
+use rdpdk::port::{DpdkPortData};
+use crate::mlx5_raw::mlx5::mlx5_priv;
+use crate::mlx5_raw::mlx5_rx::{mlx5_check_vec_rx_support, mlx5_rx_burst, mlx5_rx_burst_vec, mlx5_rxq_data};
+
+#[path = "mlx5_raw/mlx5_raw.rs"]
+pub mod mlx5_raw;
+
+pub trait Mlx5Rx {}
+
+pub struct Mlx5RxDef;
+impl Mlx5Rx for Mlx5RxDef {}
+pub struct Mlx5RxVec;
+impl Mlx5Rx for Mlx5RxVec {}
+
+unsafe impl<Rx: Mlx5Rx> Send for Mlx5Port<Rx> {}
+unsafe impl<Rx: Mlx5Rx> Sync for Mlx5Port<Rx> {}
+pub struct Mlx5Port<Rx: Mlx5Rx = Mlx5RxDef> {
+    pub port_id: u16,
+    rxq_data: *mut *mut mlx5_rxq_data,
+    __phd: PhantomData<Rx>,
+}
+
+impl Mlx5Port {
+    pub fn from(port_id: u16) -> Box<dyn DpdkPortData>{
+        let dev: *mut rdpdk::dpdk_raw::ethdev_driver::rte_eth_dev = unsafe {
+            rust_get_port_eth_device(port_id) as *mut rte_eth_dev
+        };
+
+        let mlx5_priv: &mlx5_priv =  unsafe {
+            let data = (&mut *dev).data.as_mut().unwrap();
+            (data.dev_private as *mut mlx5_priv).as_mut().unwrap()
+        };
+
+        let rxq_data = unsafe {
+            mlx5_priv.
+                dev_data.
+                as_ref()
+                .unwrap()
+                .rx_queues as *mut *mut mlx5_rxq_data
+        };
+
+        let rc = unsafe { mlx5_check_vec_rx_support(dev as *const _ as *mut _) };
+        if rc > 0 {
+            Box::new(Mlx5Port::<Mlx5RxVec> {
+                port_id: port_id,
+                rxq_data: rxq_data,
+                __phd: Default::default()
+            })
+        } else {
+            Box::new(Mlx5Port::<Mlx5RxDef> {
+                port_id: port_id,
+                rxq_data:rxq_data,
+                __phd: Default::default()
+            })
+        }
+    }
+}
+
+impl DpdkPortData for Mlx5Port<Mlx5RxDef> {
+    fn rx_burst(&mut self, queue_id: u16, pkts: *mut *mut rte_mbuf, pkt_cnt: u16) -> Result<u16, String> {
+
+        Ok(unsafe {
+            mlx5_rx_burst(
+                (*self.rxq_data).wrapping_add(queue_id as usize) as *mut c_void,
+                pkts as *mut _,
+                pkt_cnt
+            )
+        })
+    }
+}
+
+impl DpdkPortData for Mlx5Port<Mlx5RxVec> {
+    fn rx_burst(&mut self, queue_id: u16, pkts: *mut *mut rte_mbuf, pkt_cnt: u16) -> Result<u16, String> {
+        Ok(unsafe {
+            mlx5_rx_burst_vec(
+                (*self.rxq_data).wrapping_add(queue_id as usize) as *mut c_void,
+                pkts as *mut _,
+                pkt_cnt
+            )
+        })
+    }
+}
+
---
+++ port/mlx5/mlx5_raw/mlx5_raw.rs
@@ -0,0 +1,21 @@
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+
+pub mod mlx5;
+pub mod mlx5_rx;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
